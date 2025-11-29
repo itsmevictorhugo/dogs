0edea4205a6257a9cee073348ac8371944ae4212
Victor Hugo <csivictorhugo@gmail.com>
2025-11-28 23:05:14 -0300

    feat: protected route - alternativa 2

--- src/Components/Helper/ProtectedRoute.jsx
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -4,7 +4,14 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({children}) => {
   const {login} = React.useContext(UserContext);
-  return login ? children : <Navigate to="/login" />;
+  console.log(login);
+  if(login === true) {
+    return children
+  } else if (login === false) {
+    return <Navigate to="/login" />
+  } else {
+    return <></>
+  }
 }
 
 export default ProtectedRoute
