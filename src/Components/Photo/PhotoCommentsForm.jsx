ac6a6d416492028540d80933d25d4d89d0de7f42
Victor Hugo <csivictorhugo@gmail.com>
2025-11-30 21:11:25 -0300

    feat: comentario

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Enviar from '../../Assets/enviar.svg?react'
+import useFetch from '../../Hooks/useFetch'
+import Error from '../Helper/Error'
+import { COMMENT_POST } from '../../api';
+import styles from './PhotoCommentsForm.module.css'
+
+const PhotoCommentsForm = ({id, setComments}) => {
+  const [comment, setComment] = React.useState('');
+  const {request, error} = useFetch();
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    // if(!comment.trim()) {
+    //   console.log("Comentário vazio, não enviado");
+    //   return;
+    // }
+
+    const token = window.localStorage.getItem('token');
+    const {url, options} = COMMENT_POST(id, {comment}, token);
+    const {response, json} = await request(url, options);
+    console.log(json);
+    
+    if(response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea 
+        className={styles.textarea}
+        id='comment'
+        name='comment'
+        placeholder='Comente...'
+        value={comment} 
+        onChange={({target}) => setComment(target.value)}
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error}/>
+    </form>
+  )
+}
+
+export default PhotoCommentsForm
