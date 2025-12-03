63ffc27f6bf3f7f34f379e3c5c56e309aedf19ee
Victor Hugo <csivictorhugo@gmail.com>
2025-12-03 00:27:47 -0300

    perf: ajustes finais

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -39,7 +39,7 @@ function LoginPasswordReset() {
   }
 
   return (
-    <div>
+    <section className='animeLeft'>
       <Head title='Resete a senha'/>
       <h1 className='title'>Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
@@ -50,7 +50,7 @@ function LoginPasswordReset() {
         }
       </form>
       <Error error={error} />
-    </div>
+    </section>
   )
 }
 
